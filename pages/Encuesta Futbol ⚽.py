8e360799eb860edec9093a465d643f67955f444c
desarrolloqsn <desarrollo@qsocialnow.com>
2024-05-07 13:33:23 -0300

    La tercera es la vencidaa

--- pages/Encuesta Futbol ⚽.py
+++ pages/Encuesta Futbol ⚽.py
@@ -90,102 +90,102 @@ def stacked_bar_chart(x, y_data, category_names, title, colors=None):
     st.plotly_chart(fig, use_container_width=True)
     # fig = go.Figure()
 
-# while True:
-# Obtener datos actualizados de MongoDB
-data = coleccion.find()
-df = pd.DataFrame(list(data))
-df = df.drop('_id', axis=1)
+while True:
+    # Obtener datos actualizados de MongoDB
+    data = coleccion.find()
+    df = pd.DataFrame(list(data))
+    df = df.drop('_id', axis=1)
 
-# Ordenar datos
-p2 = df.groupby(['p2']).agg(
-cantidad=('p2','size')
-)
-p2 = p2.sort_values(by='cantidad', ascending=False) # Ordenar p2 según la cantidad en orden descendente
+    # Ordenar datos
+    p2 = df.groupby(['p2']).agg(
+    cantidad=('p2','size')
+    )
+    p2 = p2.sort_values(by='cantidad', ascending=False) # Ordenar p2 según la cantidad en orden descendente
 
-p3 = df.groupby(['p3']).agg(
-cantidad=('p3','size')
-)
-p2 = p2.sort_values(by='cantidad', ascending=False) # Ordenar p2 según la cantidad en orden descendente
+    p3 = df.groupby(['p3']).agg(
+    cantidad=('p3','size')
+    )
+    p2 = p2.sort_values(by='cantidad', ascending=False) # Ordenar p2 según la cantidad en orden descendente
 
-#_____________________________________
-#|--------|---------|---------|-------
-#|--------|---------|---------|-------
-#|--------|---------|---------|-------
-#|--------------Copa america----------
-#|
-#|
-#|
-#|
-#|
-#_____________________________________
-with placeholder.container(border=True):
-    with st.expander("Base de datos"):
-        st.dataframe(df)
+    #_____________________________________
+    #|--------|---------|---------|-------
+    #|--------|---------|---------|-------
+    #|--------|---------|---------|-------
+    #|--------------Copa america----------
+    #|
+    #|
+    #|
+    #|
+    #|
+    #_____________________________________
+    with placeholder.container(border=True):
+        with st.expander("Base de datos"):
+            st.dataframe(df)
 
-    pos11, pos12, pos13, pos14= st.columns(4)
+        pos11, pos12, pos13, pos14= st.columns(4)
 
-    with pos11:
-        plot_metric(
-            "Encuestados",
-            len(df),
-            prefix="",
-            suffix="",
-            show_graph=True,
-            color_graph="rgba(255, 43, 43, 0.2)",
-        )
+        with pos11:
+            plot_metric(
+                "Encuestados",
+                len(df),
+                prefix="",
+                suffix="",
+                show_graph=True,
+                color_graph="rgba(255, 43, 43, 0.2)",
+            )
 
-    with pos12:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+        with pos12:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
 
-    with pos13:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+        with pos13:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
 
-    with pos14:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+        with pos14:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
 
-    #Divisor Copa América 
-    st.markdown("<h1 style='text-align: center; font-size: 36px;'>Copa América</h1>", unsafe_allow_html=True)
-    st.markdown("<hr style='border: 1px solid #d3d3d3;'>", unsafe_allow_html=True)  # Línea divisoria
-    st.markdown("<style>div.stMarkdown {margin-top: -10px;}</style>", unsafe_allow_html=True)  # Ajuste de margen superior
+        #Divisor Copa América 
+        st.markdown("<h1 style='text-align: center; font-size: 36px;'>Copa América</h1>", unsafe_allow_html=True)
+        st.markdown("<hr style='border: 1px solid #d3d3d3;'>", unsafe_allow_html=True)  # Línea divisoria
+        st.markdown("<style>div.stMarkdown {margin-top: -10px;}</style>", unsafe_allow_html=True)  # Ajuste de margen superior
 
-    pos21, pos22, pos23, pos24= st.columns((0.4,0.2,0.2,0.2))
-    with pos21:
+        pos21, pos22, pos23, pos24= st.columns((0.4,0.2,0.2,0.2))
+        with pos21:
+
+            bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
+
+        with pos22:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+
+        with pos23:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+
+        with pos24:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+
+        pos31, pos32, pos33= st.columns((0.5,0.25,0.25))
+
+        with pos31:
+            bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
+            bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
+
+        with pos32:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+            bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
+
+
+        with pos33:
+            donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+
 
         bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
 
-    with pos22:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+        # stacked_bar_chart()
 
-    with pos23:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
+        #Divisor Champion 
+        st.markdown("<h1 style='text-align: center; font-size: 36px;'>Eurocopa</h1>", unsafe_allow_html=True)
+        st.markdown("<hr style='border: 1px solid #d3d3d3;'>", unsafe_allow_html=True)  # Línea divisoria
+        st.markdown("<style>div.stMarkdown {margin-top: -10px;}</style>", unsafe_allow_html=True)  # Ajuste de margen superior
 
-    with pos24:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
-
-    pos31, pos32, pos33= st.columns((0.5,0.25,0.25))
-
-    with pos31:
-        bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
-        bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
-
-    with pos32:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
-        bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
-
-
-    with pos33:
-        donut_chart(p3.index,p3['cantidad'],'Distribución por edad')
-
-
-    bar_chart(p2.index, p2['cantidad'], 'Cantidad por rango de edad', colors=None)
-
-    # stacked_bar_chart()
-
-    #Divisor Champion 
-    st.markdown("<h1 style='text-align: center; font-size: 36px;'>Eurocopa</h1>", unsafe_allow_html=True)
-    st.markdown("<hr style='border: 1px solid #d3d3d3;'>", unsafe_allow_html=True)  # Línea divisoria
-    st.markdown("<style>div.stMarkdown {margin-top: -10px;}</style>", unsafe_allow_html=True)  # Ajuste de margen superior
-
-    # Definir el número de columnas y la relación de anchos
-    columnas = st.columns(5)
+        # Definir el número de columnas y la relación de anchos
+        columnas = st.columns(5)
 
